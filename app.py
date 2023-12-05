be84a2a90a58b7610a460696508e671b3352553b
lorenaalv <144622521+lorenaalv@users.noreply.github.com>
2023-12-05 04:12:29 +0000

    Mon, Dec 4, 2023, 11:12 PM -05:00

--- app.py
+++ app.py
@@ -10,16 +10,13 @@ from helpers import apology, login_required
 # Configure application
 app = Flask(__name__)
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///purchases.db")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
